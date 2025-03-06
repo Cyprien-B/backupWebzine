2205aa202bbba61953be8baa49ecd32c3bac29ed
Cyprien <cyprien>
2025-03-06 16:48:28 +0100

    logique page artiste terminer viewmodel crée

--- Webzine.WebApplication/Controllers/ArtisteController.cs
+++ Webzine.WebApplication/Controllers/ArtisteController.cs
@@ -1,24 +1,51 @@
-﻿// <copyright file="ArtisteController.cs" company="Equipe 4 - BARRAND, BORDET, COPPIN, DANNEAU, ERNST, FICHET, GRANDVEAU, SADIKAJ">
+﻿// <copyright file="ApiController.cs" company="Equipe 4 - BARRAND, BORDET, COPPIN, DANNEAU, ERNST, FICHET, GRANDVEAU, SADIKAJ">
 // Copyright (c) Equipe 4 - BARRAND, BORDET, COPPIN, DANNEAU, ERNST, FICHET, GRANDVEAU, SADIKAJ. All rights reserved.
 // </copyright>
 
 namespace Webzine.WebApplication.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
+    using Webzine.Entity;
+    using Webzine.Entity.Fixtures;
+    using Webzine.WebApplication.ViewModels;
 
     /// <summary>
-    /// Contrôleur des Artistes.
+    /// Contrôleur pour gérer les actions liées aux artistes.
     /// </summary>
     public class ArtisteController : Controller
     {
         /// <summary>
-        /// Recherche artiste.
+        /// Affiche la page d'un artiste spécifique.
         /// </summary>
-        /// <param name="artiste">Nom de l'artiste demandé.</param>
-        /// <returns>Renvois la vue de l'artiste demandé.</returns>
+        /// <param name="artiste">Le nom de l'artiste à afficher.</param>
+        /// <returns>La vue de l'artiste.</returns>
         public IActionResult Index(string artiste)
         {
-            return this.View();
+            var data = new DataGenerator();
+            var fakeArtiste = data.GenerateArtiste();
+
+            var viewModel = new ArtisteViewModel.Artiste
+            {
+                IdArtiste = fakeArtiste.IdArtiste,
+                Nom = fakeArtiste.Nom,
+                Biographie = fakeArtiste.Biographie,
+                Titres = fakeArtiste.Titres,
+            };
+
+            // Regrouper les titres par album
+            var albums = fakeArtiste.Titres
+                .GroupBy(t => t.Album)
+                .Select(g => new ArtisteViewModel.Album
+                {
+                    Nom = g.Key,
+                    ImageUrl = g.First().UrlJaquette,
+                    Titres = g.ToList(),
+                })
+                .ToList();
+
+            viewModel.Albums = albums;
+
+            return this.View(viewModel);
         }
     }
 }
