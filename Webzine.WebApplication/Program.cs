d9edf2e5ff7fcc69cedc5a84a71dd579a77601f1
Diego COPPIN <diego.coppin@diiage.org>
2025-03-25 17:04:34 +0100

    Résolution de conflit entre la branche dev et la branche feature-repository-local-style

--- Webzine.WebApplication/Program.cs
+++ Webzine.WebApplication/Program.cs
@@ -1,86 +1,125 @@
-// <copyright file="Program.cs" company="Equipe 4 - BARRAND, BORDET, COPPIN, DANNEAU, ERNST, FICHET, GRANDVEAU, SADIKAJ">
+﻿// <copyright file="Program.cs" company="Equipe 4 - BARRAND, BORDET, COPPIN, DANNEAU, ERNST, FICHET, GRANDVEAU, SADIKAJ">
 // Copyright (c) Equipe 4 - BARRAND, BORDET, COPPIN, DANNEAU, ERNST, FICHET, GRANDVEAU, SADIKAJ. All rights reserved.
 // </copyright>
+
+using Webzine.Repository.Contracts;
+using Webzine.Repository.Local;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Webzine.EntityContext.Dbcontext;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Gestion de la connection a SQLite
-builder.Services.AddDbContext<SQLiteContext>(options =>
-    options.UseSqlite(
-        "Data Source=Webzine.db",
-        b => b.MigrationsAssembly("Webzine.WebApplication")));
-
-builder.Services.AddControllers();
-
-// Ajoute les services necessaires pour permettre l'utilisation des
-// controllers avec des vues.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-// Gestion des erreurs 404
-app.UseStatusCodePages(context =>
+/// <summary>
+/// Contient le point d'entr�e principal de l'application.
+/// </summary>
+public static class Program
 {
-    if (context.HttpContext.Response.StatusCode == 404)
+    /// <summary>
+    /// Obtient ou d�finit le builder de l'application.
+    /// </summary>
+    public static WebApplicationBuilder? Builder { get; set; } = null;
+
+    /// <summary>
+    /// Obtient ou d�finit l'application compil�e par le builder.
+    /// </summary>
+    public static WebApplication? App { get; set; } = null;
+
+    /// <summary>
+    /// Point d'entr�e principal de l'application.
+    /// </summary>
+    /// <param name="args">Les arguments de ligne de commande pass�s au programme.</param>
+    public static void Main(string[] args)
     {
-        context.HttpContext.Response.Redirect("/Home/NotFound404");
+        Builder = WebApplication.CreateBuilder(args);
+
+        AddDependenciesInjections();
+
+        // Gestion de la connection a SQLite
+        Builder.Services.AddDbContext<SQLiteContext>(options =>
+            options.UseSqlite(
+                "Data Source=Webzine.db",
+                b => b.MigrationsAssembly("Webzine.WebApplication")));
+
+        Builder.Services.AddControllers();
+        Builder.Services.AddControllersWithViews();
+
+        App = Builder.Build();
+
+        ConfigureMiddleware();
+
+        ConfigureCustomRoutes();
+
+        App.Run();
     }
 
-    return Task.CompletedTask;
-});
+    private static void AddDependenciesInjections()
+    {
+        Builder!.Services.AddScoped<IStyleRepository, LocalStyleRepository>();
+    }
 
-app.MapControllerRoute(
-    name: "page",
-    pattern: "page/{page:int}",
-    defaults: new { controller = "Home", action = "Index" });
+    private static void ConfigureCustomRoutes()
+    {
+        App!.MapControllerRoute(
+        name: "page",
+        pattern: "page/{page:int}",
+        defaults: new { controller = "Home", action = "Index" });
 
-app.MapControllerRoute(
-    name: "titre-id",
-    pattern: "titre/{id:int}",
-    defaults: new { controller = "Titre", action = "Index" });
+        App!.MapControllerRoute(
+        name: "titre-id",
+        pattern: "titre/{id:int}",
+        defaults: new { controller = "Titre", action = "Index" });
 
-app.MapControllerRoute(
-    name: "titre-style",
-    pattern: "titres/style/{nomStyle}",
-    defaults: new { controller = "Titre", action = "Style" });
+        App!.MapControllerRoute(
+        name: "titre-style",
+        pattern: "titres/style/{nomStyle}",
+        defaults: new { controller = "Titre", action = "Style" });
 
-app.MapControllerRoute(
-    name: "artiste",
-    pattern: "artiste/{nomArtiste}",
-    defaults: new { controller = "Artiste", action = "Index" });
+        App!.MapControllerRoute(
+        name: "artiste",
+        pattern: "artiste/{nomArtiste}",
+        defaults: new { controller = "Artiste", action = "Index" });
 
-app.MapControllerRoute(
-    name: "titres-administration",
-    pattern: "administration/artistes",
-    defaults: new { area = "Administration", controller = "Artiste", action = "Index" });
+        App!.MapControllerRoute(
+        name: "titres-administration",
+        pattern: "administration/artistes",
+        defaults: new { area = "Administration", controller = "Artiste", action = "Index" });
 
-app.MapControllerRoute(
-    name: "titres-administration",
-    pattern: "administration/commentaires",
-    defaults: new { area = "Administration", controller = "Commentaire", action = "Index" });
+        App!.MapControllerRoute(
+        name: "titres-administration",
+        pattern: "administration/commentaires",
+        defaults: new { area = "Administration", controller = "Commentaire", action = "Index" });
 
-app.MapControllerRoute(
-    name: "titres-administration",
-    pattern: "administration/styles",
-    defaults: new { area = "Administration", controller = "Style", action = "Index" });
+        App!.MapControllerRoute(
+        name: "titres-administration",
+        pattern: "administration/styles",
+        defaults: new { area = "Administration", controller = "Style", action = "Index" });
 
-app.MapControllerRoute(
-    name: "titres-administration",
-    pattern: "administration/titres",
-    defaults: new { area = "Administration", controller = "Titre", action = "Index" });
+        App!.MapControllerRoute(
+        name: "titres-administration",
+        pattern: "administration/titres",
+        defaults: new { area = "Administration", controller = "Titre", action = "Index" });
 
-app.MapControllerRoute(
-    name: "areas",
-    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+        App!.MapControllerRoute(
+        name: "areas",
+        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
-app.MapDefaultControllerRoute();
+        App!.MapDefaultControllerRoute();
+    }
 
-app.Run();
+    private static void ConfigureMiddleware()
+    {
+        App.UseStaticFiles();
+        App.UseRouting();
+
+        // Gestion des erreurs 404
+        App.UseStatusCodePages(context =>
+        {
+            if (context.HttpContext.Response.StatusCode == 404)
+            {
+                context.HttpContext.Response.Redirect("/Home/NotFound404");
+            }
+
+            return Task.CompletedTask;
+        });
+    }
+}
